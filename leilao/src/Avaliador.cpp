84d6012a4479852ba3d4aa88fc3315b33033825c
Camila Mastellini Oliveira <camila_mastellini@hotmail.com>
2021-10-09 17:20:28 -0300

    Aula 02 - Catch2

--- leilao/src/Avaliador.cpp
+++ leilao/src/Avaliador.cpp
@@ -11,10 +11,15 @@ void Avaliador::avalia(Leilao leilao) {
         if(lance.recuperaValor() > maiorValor) {
             maiorValor = lance.recuperaValor();
         }
-
+        if(lance.recuperaValor() < menorValor) {
+            menorValor = lance.recuperaValor();
+        }
     }
 }
 
 float Avaliador::recuperaMaiorValor() const {
     return maiorValor;
+}
+float Avaliador::recuperaMenorValor() const {
+    return menorValor;
 }
--- leilao/tests/AvaliadorTeste.cpp
+++ leilao/tests/AvaliadorTeste.cpp
@@ -4,12 +4,13 @@
 
 #include "../src/Avaliador.h"
 #include<iostream>
+#define CATCH_CONFIG_MAIN
+#include "../catch2.h"
 
-int main() {
-
+TEST_CASE("Deve recuperar maior lance de leilao em ordem crescente") {
     // Preparando -- Arrange -- Given
-    Lance primeiroLance(Usuario("Vinicius Dias"),1000);
-    Lance segundoLance(Usuario("Ana Maria"),2000);
+    Lance primeiroLance(Usuario("Vinicius Dias"), 1000);
+    Lance segundoLance(Usuario("Ana Maria"), 2000);
     Leilao leilao("Fiat 147 0km");
     leilao.recebeLance(primeiroLance);
     leilao.recebeLance(segundoLance);
@@ -17,13 +18,48 @@ int main() {
     Avaliador leiloeiro;
     // Executando o c?digo a ser testado -- Act -- When
     leiloeiro.avalia(leilao);
-
-    float valorEsperado = 2000;
     // Verificando se deu certo -- Assert -- Then
-    if(valorEsperado == leiloeiro.recuperaMaiorValor()) {
-        std::cout << "Teste ok!" << std::endl;
-    } else {
-        std::cout << "Teste falhou..." << std::endl;
-    }
-    return 0;
+    REQUIRE(2000 == leiloeiro.recuperaMaiorValor());
+}
+TEST_CASE("Deve recuperar maior lance de leilao em ordem decrescente") {
+    // Preparando -- Arrange -- Given
+    Lance primeiroLance(Usuario("Ana Maria"), 2000);
+    Lance segundoLance(Usuario("Vinicius Dias"), 1000);
+    Leilao leilao("Fiat 147 0km");
+    leilao.recebeLance(primeiroLance);
+    leilao.recebeLance(segundoLance);
+
+    Avaliador leiloeiro;
+    // Executando o c?digo a ser testado -- Act -- When
+    leiloeiro.avalia(leilao);
+    // Verificando se deu certo -- Assert -- Then
+    REQUIRE(2000 == leiloeiro.recuperaMaiorValor());
+}
+TEST_CASE("Deve recuperar menor lance de leilao em ordem crescente") {
+    // Preparando -- Arrange -- Given
+    Lance primeiroLance(Usuario("Vinicius Dias"), 1000);
+    Lance segundoLance(Usuario("Ana Maria"), 2000);
+    Leilao leilao("Fiat 147 0km");
+    leilao.recebeLance(primeiroLance);
+    leilao.recebeLance(segundoLance);
+
+    Avaliador leiloeiro;
+    // Executando o c?digo a ser testado -- Act -- When
+    leiloeiro.avalia(leilao);
+    // Verificando se deu certo -- Assert -- Then
+    REQUIRE(1000 == leiloeiro.recuperaMenorValor());
+}
+TEST_CASE("Deve recuperar menor lance de leilao em ordem decrescente") {
+    // Preparando -- Arrange -- Given
+    Lance primeiroLance(Usuario("Ana Maria"), 2000);
+    Lance segundoLance(Usuario("Vinicius Dias"), 1000);
+    Leilao leilao("Fiat 147 0km");
+    leilao.recebeLance(primeiroLance);
+    leilao.recebeLance(segundoLance);
+
+    Avaliador leiloeiro;
+    // Executando o c?digo a ser testado -- Act -- When
+    leiloeiro.avalia(leilao);
+    // Verificando se deu certo -- Assert -- Then
+    REQUIRE(1000 == leiloeiro.recuperaMenorValor());
 }
